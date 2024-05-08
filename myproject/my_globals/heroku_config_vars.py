7eac1ef77d5c70a295adc48f6856120c09bca68e
Muhammad Hamza <hamzaashraf@systemheuristics.com>
2024-05-08 13:17:16 +0500

    Update heroku_config_vars.py

--- myproject/my_globals/heroku_config_vars.py
+++ myproject/my_globals/heroku_config_vars.py
@@ -1,21 +1,14 @@
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 
-# host=os.getenv('HOST')
-# db=os.getenv('DB')
-
-try:
-    import environ
-
-    env = environ.Env()
-    environ.Env.read_env()
+try:   
     mongo_uri=env.str('MONGO_URI')
+except:
+    mongo_uri = 'mongodb://dev:oaiipv8wyg912urtn@202.163.113.3:27017'
+    
+try:
     db=env.str('DB')
 except:
-    from dotenv import load_dotenv
-    import os
-    load_dotenv()
-    mongo_uri=os.getenv('MONGO_URI')
-    db=os.getenv('DB')
+    db = 'sea-turtle'
