5f479b468ccbee8bbf8334df48c6b0d92fe627e1
Muhammad Hamza <hamzaashraf@systemheuristics.com>
2024-05-09 15:46:12 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -13,16 +13,20 @@ def login(request):
     try:
         if request.method == 'POST':
             if login_helper(request):
+                print('login successful')
                 username=request.POST.get('username')
                 request.session['username'] = username
                 result = {"success": 1, "message": "Login Success","error_code":0}
                 return render(request,'orders.html',result)
             else:
+                print('login failed')
                 result = {"success": 0, "message": "Incorrect Password or Username","error_code":1}
                 return render(request,'login.html',result)
         elif request.method == 'GET':
+            print('login page')
             return render(request,'login.html')
     except Exception as e:
+        print('error on login page')
         result = {"success": 0, "message": str(e),"error_code":1}
         return JsonResponse(result,safe=False,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
@@ -32,18 +36,23 @@ def order_list(request):
     try:
         username = request.session.get('username')
         if not username:
+            print('user not found')
             result = {"success": 0, "message": "User not found","error_code":1}
             return JsonResponse(result, safe=False,status=status.HTTP_404_NOT_FOUND)
         if request.method=='POST':
             orders=orders_list_helper(request)
             if len(orders)>0:
+                print('orders found')
                 return JsonResponse(orders, safe=False,status=status.HTTP_200_OK)
             else:
-                result = {"success": 0, "message": "Incorrect Password or Username","error_code":1}
+                print('zero orders')
+                result = {"success": 0, "message": "0 orders found","error_code":1}
                 return JsonResponse(result, safe=False,status=status.HTTP_404_NOT_FOUND)
+        print('invalid HTTP Request')
         result = {"success": 0, "message": "Invalid HTTP Request","error_code":1}
         return JsonResponse(result, safe=False,status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
+        print('error on orders page')
         result = {"success": 0, "message": str(e),"error_code":1}
         return JsonResponse(result,safe=False,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -51,6 +60,7 @@ def order_list(request):
 def logout(request):
     username = request.session.get('username')
     if username:
+        print('logout')
         request.session.flush()
         return redirect('login')
     else:
