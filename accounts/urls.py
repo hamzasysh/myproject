819165b5e580aa6c9bf13332ac4abb30d1a33f24
Muhammad Hamza <hamzaashraf@systemheuristics.com>
2024-05-13 17:12:17 +0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('orders/', views.order_list, name='orders'),
     path('logout/', views.logout, name='logout'),
-    path('add_data/', views.add_data, name='add_data'),
+    path('add_data', views.add_data, name='add_data'),
 
 ]
